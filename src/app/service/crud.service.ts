60f6b094539494a6d39e51238500dd494f3c37b6
Markosgt7 <markos.guate7@gmail.com>
2023-07-27 10:45:36 -0600

    create the method on service.ts to consume from db.json

--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { Task } from '../model/task';//se importa la clase creada previamente
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +13,19 @@ export class CrudService {
   constructor(private http : HttpClient) { 
     this.serviceURL = "http://localhost:3000/tasks";
   }
+
+  //define method to conect to the json-server api
+  addTask(task: Task) : Observable<Task>{
+    return this.http.post<Task>(this.serviceURL,task)
+  }
+  getAllTasks() : Observable<Task[]>{
+    return this.http.get<Task[]>(this.serviceURL)
+  }
+  deleteTask(task: Task) : Observable<Task>{
+    return this.http.delete<Task>(this.serviceURL+'/'+task.id)
+  }
+  editTask(task: Task) : Observable<Task>{
+    return this.http.put<Task>(this.serviceURL+'/'+task.id,task)
+  }
+
 }
